c1ef32e1e9d6b749dcf474631df434df3114fdbe
Miguel Ángel Valentín Naranjo <mavnaranjo@gmail.com>
2025-03-05 19:19:55 +0100

    Allow to generate a pitch from an interval

--- src/app/domain/theory/pitch/pitch.spec.ts
+++ src/app/domain/theory/pitch/pitch.spec.ts
@@ -1,3 +1,4 @@
+import { Interval } from '../interval/interval';
 import { Pitch } from './pitch';
 import { PitchAlteration } from './pitch-alteration';
 
@@ -45,6 +46,106 @@ describe('Pitch', () => {
                 const fromPitch = Pitch.fromString(testCase.from);
                 const toPitch = Pitch.fromString(testCase.to);
                 expect(fromPitch.distance(toPitch)).toBe(testCase.distance);
+            });
+        });
+    });
+
+    describe('Create from interval', () => {
+        const testCases = [
+            { from: 'C', interval: 'd1', expected: 'C♭' },
+            { from: 'C', interval: 'P1', expected: 'C' },
+            { from: 'C', interval: 'A1', expected: 'C♯' },
+            { from: 'C', interval: 'd2', expected: 'D𝄫' },
+            { from: 'C', interval: 'm2', expected: 'D♭' },
+            { from: 'C', interval: 'M2', expected: 'D' },
+            { from: 'C', interval: 'A2', expected: 'D♯' },
+            { from: 'C', interval: 'd3', expected: 'E𝄫' },
+            { from: 'C', interval: 'm3', expected: 'E♭' },
+            { from: 'C', interval: 'M3', expected: 'E' },
+            { from: 'C', interval: 'A3', expected: 'E♯' },
+            { from: 'C', interval: 'd4', expected: 'F♭' },
+            { from: 'C', interval: 'P4', expected: 'F' },
+            { from: 'C', interval: 'A4', expected: 'F♯' },
+            { from: 'C', interval: 'd5', expected: 'G♭' },
+            { from: 'C', interval: 'P5', expected: 'G' },
+            { from: 'C', interval: 'A5', expected: 'G♯' },
+            { from: 'C', interval: 'd6', expected: 'A𝄫' },
+            { from: 'C', interval: 'm6', expected: 'A♭' },
+            { from: 'C', interval: 'M6', expected: 'A' },
+            { from: 'C', interval: 'A6', expected: 'A♯' },
+            { from: 'C', interval: 'd7', expected: 'B𝄫' },
+            { from: 'C', interval: 'm7', expected: 'B♭' },
+            { from: 'C', interval: 'M7', expected: 'B' },
+            { from: 'C', interval: 'A7', expected: 'B♯' },
+            { from: 'C', interval: 'd8', expected: 'C♭' },
+            { from: 'C', interval: 'P8', expected: 'C' },
+            { from: 'C', interval: 'A8', expected: 'C♯' },
+
+            { from: 'C♯', interval: 'd1', expected: 'C' },
+            { from: 'C♯', interval: 'P1', expected: 'C♯' },
+            { from: 'C♯', interval: 'A1', expected: 'C𝄪' },
+            { from: 'C♯', interval: 'd2', expected: 'D♭' },
+            { from: 'C♯', interval: 'm2', expected: 'D' },
+            { from: 'C♯', interval: 'M2', expected: 'D♯' },
+            { from: 'C♯', interval: 'A2', expected: 'D𝄪' },
+            { from: 'C♯', interval: 'd3', expected: 'E♭' },
+            { from: 'C♯', interval: 'm3', expected: 'E' },
+            { from: 'C♯', interval: 'M3', expected: 'E♯' },
+            { from: 'C♯', interval: 'A3', expected: 'E𝄪' },
+            { from: 'C♯', interval: 'd4', expected: 'F' },
+            { from: 'C♯', interval: 'P4', expected: 'F♯' },
+            { from: 'C♯', interval: 'A4', expected: 'F𝄪' },
+            { from: 'C♯', interval: 'd5', expected: 'G' },
+            { from: 'C♯', interval: 'P5', expected: 'G♯' },
+            { from: 'C♯', interval: 'A5', expected: 'G𝄪' },
+            { from: 'C♯', interval: 'd6', expected: 'A♭' },
+            { from: 'C♯', interval: 'm6', expected: 'A' },
+            { from: 'C♯', interval: 'M6', expected: 'A♯' },
+            { from: 'C♯', interval: 'A6', expected: 'A𝄪' },
+            { from: 'C♯', interval: 'd7', expected: 'B♭' },
+            { from: 'C♯', interval: 'm7', expected: 'B' },
+            { from: 'C♯', interval: 'M7', expected: 'B♯' },
+            { from: 'C♯', interval: 'A7', expected: 'B𝄪' },
+            { from: 'C♯', interval: 'd8', expected: 'C' },
+            { from: 'C♯', interval: 'P8', expected: 'C♯' },
+            { from: 'C♯', interval: 'A8', expected: 'C𝄪' },
+
+            { from: 'B♭', interval: 'd1', expected: 'B𝄫' },
+            { from: 'B♭', interval: 'P1', expected: 'B♭' },
+            { from: 'B♭', interval: 'A1', expected: 'B' },
+            { from: 'B♭', interval: 'd2', expected: 'C𝄫' },
+            { from: 'B♭', interval: 'm2', expected: 'C♭' },
+            { from: 'B♭', interval: 'M2', expected: 'C' },
+            { from: 'B♭', interval: 'A2', expected: 'C♯' },
+            { from: 'B♭', interval: 'd3', expected: 'D𝄫' },
+            { from: 'B♭', interval: 'm3', expected: 'D♭' },
+            { from: 'B♭', interval: 'M3', expected: 'D' },
+            { from: 'B♭', interval: 'A3', expected: 'D♯' },
+            { from: 'B♭', interval: 'd4', expected: 'E𝄫' },
+            { from: 'B♭', interval: 'P4', expected: 'E♭' },
+            { from: 'B♭', interval: 'A4', expected: 'E' },
+            { from: 'B♭', interval: 'd5', expected: 'F♭' },
+            { from: 'B♭', interval: 'P5', expected: 'F' },
+            { from: 'B♭', interval: 'A5', expected: 'F♯' },
+            { from: 'B♭', interval: 'd6', expected: 'G𝄫' },
+            { from: 'B♭', interval: 'm6', expected: 'G♭' },
+            { from: 'B♭', interval: 'M6', expected: 'G' },
+            { from: 'B♭', interval: 'A6', expected: 'G♯' },
+            { from: 'B♭', interval: 'd7', expected: 'A𝄫' },
+            { from: 'B♭', interval: 'm7', expected: 'A♭' },
+            { from: 'B♭', interval: 'M7', expected: 'A' },
+            { from: 'B♭', interval: 'A7', expected: 'A♯' },
+            { from: 'B♭', interval: 'd8', expected: 'B𝄫' },
+            { from: 'B♭', interval: 'P8', expected: 'B♭' },
+            { from: 'B♭', interval: 'A8', expected: 'B' },
+        ];
+
+        testCases.forEach(testCase => {
+            it(`${testCase.interval} from ${testCase.from} should be ${testCase.expected}`, () => {
+                const fromPitch = Pitch.fromString(testCase.from);
+                const interval = Interval.fromString(testCase.interval);
+                const toPitch = Pitch.fromInterval(fromPitch, interval);
+                expect(toPitch.toString()).toBe(testCase.expected);
             });
         });
     });
--- src/app/domain/theory/pitch/pitch.ts
+++ src/app/domain/theory/pitch/pitch.ts
@@ -1,5 +1,6 @@
 import { PitchName } from './pitch-name';
 import { PitchAlteration, PitchAlterationPresentation } from './pitch-alteration';
+import { Interval } from '../interval/interval';
 
 export class Pitch {
     readonly pitch: PitchName;
@@ -22,10 +23,34 @@ export class Pitch {
         const pitchName = PitchName[match[1] as keyof typeof PitchName];
 
         const alterationString = (match[2] ?? '♮') as PitchAlterationPresentation;
-        const alterationKey = Object.keys(PitchAlterationPresentation)[Object.values(PitchAlterationPresentation).indexOf(alterationString)];
-        const pitchAlteration = PitchAlteration[alterationKey as keyof typeof PitchAlteration];
+        const alterationIndex = Object.values(PitchAlterationPresentation).indexOf(alterationString);
+        const alterationKey = Object.keys(PitchAlterationPresentation)[alterationIndex] as keyof typeof PitchAlteration;
+        const pitchAlteration = PitchAlteration[alterationKey];
 
         return new Pitch(pitchName, pitchAlteration);
+    }
+
+    static fromInterval(from: Pitch, interval: Interval): Pitch {
+        const pitchNameKeys = Object.keys(PitchName).filter(key => isNaN(Number(key)));
+
+        const fromPitchKey = PitchName[from.pitch] as keyof typeof PitchName;
+        const fromPitchIndex = pitchNameKeys.indexOf(fromPitchKey);
+
+        const pitchIndex = (fromPitchIndex + interval.number - 1) % pitchNameKeys.length;
+        const pitchKey = pitchNameKeys[pitchIndex] as keyof typeof PitchName;
+        const pitchName = PitchName[pitchKey];
+
+        const pitch = new Pitch(pitchName);
+
+        let alterationDistance = interval.distance - from.distance(pitch);
+        if (alterationDistance > PitchAlteration.DOUBLE_SHARP) {
+            alterationDistance -= 12;
+        }
+        const alterationValue = (alterationDistance % 12) as PitchAlteration;
+        const alterationKey = PitchAlteration[alterationValue] as keyof typeof PitchAlteration;
+        const alteration = PitchAlteration[alterationKey];
+
+        return new Pitch(pitchName, alteration);
     }
 
     distance(other: Pitch) {
